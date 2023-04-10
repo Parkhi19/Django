c0488f8d3d8ca72572fc0e91711fa589dd8dd2c4
Parkhi <Parkhigoyal46@gmail.com>
2023-04-10 11:02:42 +0530

    created dummy database for blog posts

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post',
+        'content': 'First Post content',
+        'date_posted': 'April, 04, 2023'
+    },
+    {
+        'author': 'Parkhi Goyal',
+        'title': 'Blog Post 2',
+        'content': 'First Post content',
+        'date_posted': 'April, 04, 2023'
+    }
+]
+
 
 def home(request):
- return render(request, 'blog/home.html')
+    return render(request, 'blog/home.html')
+
 
 def about(request):
     return render(request, 'blog/about.html')
